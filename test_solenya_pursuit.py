44dfbf28a2ff949df1c0e4b47103afcfcd2ef0de
Sergey Melnikov <tembelis@bk.ru>
2024-05-04 23:39:13 +0300

    Поменял логику негативных тест-кейсов (теперь проверяется появление текста об отсутствии результатов)

--- test_solenya_pursuit.py
+++ test_solenya_pursuit.py
@@ -10,8 +10,10 @@ def test_solenya_google_search():
 
 def test_solenya_google_search_negative():
     browser.open("https://google.ru")
-    browser.element('[name="q"]').should(be.blank).type('Купить соленья').press_enter()
-    browser.element('#search').should(have.no.text('Неурожайный год мог принести в дома голод, а мог и не принести'))
+    browser.element('[name="q"]').should(be.blank).type('соленьясоленьясоленьясоленьясоленьясоленья'
+                                                        'соленьясоленьясоленьясоленья').press_enter()
+    browser.element('#botstuff').should(have.text('Your search - соленьясоленьясоленьясоленьясоленья'
+                                                  'соленьясоленьясоленьясоленьясоленья - did not match any documents'))
 
 
 def test_solenya_yandex_search():
@@ -23,6 +25,6 @@ def test_solenya_yandex_search():
 
 def test_solenya_yandex_search_negative():
     browser.open("https://ya.ru")
-    browser.element('[name="text"]').should(be.blank).type('Купить соленья').press_enter()
-    browser.element('#search-result').should(have.no.text('Соленья - это полезный и вкусный продукт, который '
-                                                          'можно купить, а можно и не купить'))
+    browser.element('[name="text"]').should(be.blank).type('соленьясоленьясоленьясоленьясоленьясоленья'
+                                                           'соленьясоленьясоленьясоленья').press_enter()
+    browser.element('.EmptySearchResults').should(have.text('Ничего не нашли'))
